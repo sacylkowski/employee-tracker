188481267cd08d935304a0850e79be3ab02808ae
sacylkowski <sacylkowski@gmail.com>
2022-04-23 10:59:23 -0500

    Added main prompt

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
+--creating the database--
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+-- using the inventory database --
+USE employee_db;
+
 DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
---
+++ server.js
@@ -0,0 +1,62 @@
+const inquirer = require("inquirer");
+const db = require("./db/connection");
+
+async function askMainQuestions() {
+    const { choice } = await prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Departments",
+                    value: "view_departments"
+                },
+                {
+                    name: "View All Roles",
+                    value: "view_roles"
+                },
+                {
+                    name: "View All Employees",
+                    value: "view_employees"
+                },
+                {
+                    name: "Add A Department",
+                    value: "add_department"
+                },
+                {
+                    name: "Add A Role",
+                    value: "add_role"
+                },
+                {
+                    name: "Add An Employee",
+                    value: "add_employee"
+                },
+                {
+                    name: "Update An Employee Role",
+                    value: "update_employee"
+                },
+                {
+                    name: "Quit",
+                    value: "QUIT",
+                },
+            ],       
+         },
+    ])
+switch (choice) {
+    case "view_departments":
+        return viewAllDepartments();
+    default:
+        return quit();
+}
+
+}
+
+async function viewAllDepartments() {
+    const 
+}
+
+function quit() {
+    console.log("Bye")
+    process.exit();
+};
